2e5d89b0fa15f178c3afe5a4e4f75400486c8712
sikorskayaX <sikorskaya02@gmail.com>
2023-11-02 03:12:15 +0300

    Add canvas functionality and style updates
    - Added canvas functionality for drawing
    - Implemented event listeners for mouse actions
    - Updated canvas size and line width
    - Added color buttons and range control
    - Styled the canvas and color controls

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-﻿
+﻿const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.height = 500;
+canvas.width = 800;
+
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = 'black';
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
+
+function onMouseMove(event){
+    x = event.offsetX;
+    y = event.offsetY;
+    if (!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }
+    else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+
+if(canvas){
+    canvas.addEventListener('mousemove', onMouseMove)
+    canvas.addEventListener('mousedown', onMouseDown);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+}
