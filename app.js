c58d792b759e5faf6d923482ecc2cda72301ba54
sikorskayaX <sikorskaya02@gmail.com>
2023-11-03 18:38:30 +0300

    Refactor app.js and index.html files
    - Refactored app.js to improve code readability and organization
    - Renamed variables and functions for better clarity
    - Added initial color constant for stroke and fill
    - Added functionality for filling the canvas with selected color
    - Added event listeners for range input, mode button, and save button
    - Refactored index.html to fix typo in id attribute of range input
    - Renamed mode and save button ids for consistency

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 ﻿const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
+
+const INITIAL_COLOR = 'rgb(20, 20, 20)';
 
 canvas.height = 500;
 canvas.width = 800;
 
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.lineWidth = 2.5;
-ctx.strokeStyle = 'rgb(20, 20, 20)';
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -35,17 +44,66 @@ function onMouseDown(event){
     painting = true;
 }
 
+function hanndleCM(event){
+    event.preventDefault();
+}
+
+function handleSave(){
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = "Webpaint [Export]";
+    link.click();
+}
 
 if(canvas){
     canvas.addEventListener('mousemove', onMouseMove)
     canvas.addEventListener('mousedown', onMouseDown);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', hanndleCM);
 }
 
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const rangeValue = event.target.value;
+    ctx.lineWidth = rangeValue;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = 'Bucket';
+    }
+    else{
+        filling = true;
+        mode.innerText = 'Drawing';
+        
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
+
+if (range){
+    range.addEventListener('input', handleRangeChange);
+}
+
+if (mode){
+    mode.addEventListener('click', handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener('click', handleSave);
+}
